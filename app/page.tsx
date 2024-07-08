c9b6d93447c1ee9277fe1f051ff6b9f54646900a
Fikri Ibnu Fajar <fikriibnuf10@gmail.com>
2024-07-08 14:51:41 +0700

    pertemuan 3 50%

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,22 @@ export default function Home() {
   return (
     <>
       <div>
-        <h1 className="mt-10 flex justify-center text-2xl">Kampus Gratis</h1>
+        <header>
+          <div className="flex justify-between m-7 border-b-indigo-500">
+            <div className="flex flex-row gap-2">
+              <div>Home</div>
+              <div>About Us</div>
+            </div>
+            <div className="text-2xl">Kampus Gratis</div>
+            <div className="flex flex-row gap-2">
+              <div>Course</div>
+              <div>Blog</div>
+            </div>
+          </div>
+        </header>
+        {/* <h1 className="mt-10 flex justify-center text-2xl">Kampus Gratis</h1> */}
         <h1 className="mt-10 flex justify-center text-5xl">Hello World</h1>
-        <div className="w-full flex flex-row justify-items-center">
+        <div className="mt-10 text-2xl w-full flex flex-row justify-around">
           <div>
             <Link href="/post/1">
               <p>Post 1</p>
@@ -23,6 +36,12 @@ export default function Home() {
             </Link>
           </div>
         </div>
+
+        <footer className="">
+          <div className="my-20 text-3xl bg-blue-500">
+            selamat datang di kampus gratis
+          </div>
+        </footer>
       </div>
     </>
   );
