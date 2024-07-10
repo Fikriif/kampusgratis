c556060b5395da0b63dbcf25b89d5d83aa7dea66
Fikri Ibnu Fajar <fikriibnuf10@gmail.com>
2024-07-10 10:27:57 +0700

    Fiks Landing Page

--- app/component/homepage/Footer.tsx
+++ app/component/homepage/Footer.tsx
@@ -1,11 +1,50 @@
-import React from "react";
+import Image from "next/image";
 
 const Footer = () => {
   return (
     <div>
-      <footer className="">
-        <div className="mt-60 my-20 text-3xl bg-blue-500 flex justify-center">
-          selamat datang di kampus gratis
+      <footer className="bg-dark text-white">
+        <div className="mt-60 pt-20 pb-5">
+          <div className="flex justify-around">
+            <div>
+              <div className="text-2xl pb-2 font-semibold text-primary">
+                Kampus Gratis
+              </div>
+              <div className="text-xl font-semibold pb-1">Hubungi Kami</div>
+              <div className="text-xs text-slate-500">fikribnuf@gmail.com</div>
+              <div className="text-xs text-slate-500">
+                Jl. Raya Cirendeu No. 61, Ruko Abu-Abu Sebelah PPAT
+              </div>
+            </div>
+            <div>
+              <div className="text-xl font-semibold pb-2">Kategori Tulisan</div>
+              <div className="text-xs text-slate-500">
+                <div>FullStack Development</div>
+                <div>UI/UX Design</div>
+                <div>Data Science</div>
+              </div>
+            </div>
+            <div>
+              <div className="text-xl pb-2 font-semibold">Tautan</div>
+              <div className="text-xs text-slate-500">
+                <div>Home</div>
+                <div>Blog</div>
+                <div>Course</div>
+                <div>About</div>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex justify-center gap-2 pt-20">
+            <Image src="/twitter.png" alt="none" width={30} height={30} />
+            <Image src="/facebook.png" alt="none" width={30} height={30} />
+            <Image src="/insta.png" alt="none" width={30} height={30} />
+            <Image src="/linkedin.png" alt="none" width={30} height={30} />
+            <Image src="/whatsapp.png" alt="none" width={30} height={30} />
+          </div>
+          <div className="flex justify-center text-slate-500 text-xs pt-4">
+            Dibuat Oleh Fikri Ibnu Fajar Menggunakan Tailwind CSS dan Nextjs 14
+          </div>
         </div>
       </footer>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
               <div className="text-slate-500 pb-10 lg:text-base sm:text-xs">
                 Platform belajar gratis pertama di Indonesia, belajar mudah
                 dengan kampus gratis bisa diakses siapapun khususnya kaum
-                marginal
+                marginal.
               </div>
               <a
                 href="#"
@@ -32,56 +32,107 @@ export default function Home() {
             </div>
           </div>
         </section>
+        <div className="text-dark text-xl font-semibold flex justify-center py-10">
+          Berikut ini beberapa program bootcamp di{" "}
+          <span className="text-primary">Kampus Gratis</span>
+        </div>
         <section>
           <div className="mt-10 text-2xl w-full flex md:flex-col lg:flex-row justify-around">
-            <div className="max-w-64 p-2 border border-solid border-2 border-slate-600 rounded-lg">
-              <Link href="/post/Fullstack">
+            <div className="max-w-64 p-2 border border-solid border-2 border-primary rounded-lg">
+              <div>
                 <div>
-                  <div>
-                    <Image
-                      src="/fullstack1.png"
-                      alt="logo"
-                      width={250}
-                      height={250}
-                      className="rounded-lg"
-                    />
-                  </div>
-                  <div className="pt-2 text-sm text-primary font-semibold">
-                    Bootcamp
-                  </div>
-                  <div className="pt-3 font-semibold text-dark text-xl">
-                    FullStack Development
-                  </div>
-                  <div className="pt-1 pb-3 text-xs">
-                    fast-track program untuk belajar dan mulai karir di bbidang
-                    coding dengan fasilitas koneksi kerja seumur hidup
-                  </div>
-                  <a
-                    href="/"
+                  <Image
+                    src="/fullstack1.png"
+                    alt="logo"
+                    width={250}
+                    height={250}
+                    className="rounded-lg"
+                  />
+                </div>
+                <div className="pt-2 text-sm text-primary font-semibold">
+                  Bootcamp
+                </div>
+                <div className="pt-3 font-semibold text-dark text-xl">
+                  FullStack Development
+                </div>
+                <div className="pt-1 pb-3 text-xs">
+                  fast-track program untuk belajar dan mulai karir di bbidang
+                  coding dengan fasilitas koneksi kerja seumur hidup.
+                </div>
+                <div className="py-1">
+                  <Link
+                    href="/post/Fullstack"
                     className="bg-primary py-1 flex justify-center rounded-lg hover:shadow-lg hover:bg-teal-400 text-base text-white font-bold"
                   >
                     Lihat Detail
-                  </a>
+                  </Link>
                 </div>
-              </Link>
+              </div>
             </div>
             <div>
-              <Link href="/post/2">
-                <div className="border-solid">
-                  <div>
-                    <Image src="" alt="logo" width={150} height={150} />
-                  </div>
-                  <p>Design Interior</p>
-                </div>
-              </Link>
-            </div>
-            <div>
-              <Link href="/post/3">
+              <div className="max-w-64 p-2 border border-solid border-2 border-primary rounded-lg">
                 <div>
-                  <Image src="" alt="logo" width={150} height={150} />
+                  <Image
+                    src="/designer.jpg"
+                    alt="logo"
+                    width={250}
+                    height={250}
+                    className="rounded-lg"
+                  />
                 </div>
-                <p>Data Analist</p>
-              </Link>
+                <div className="text-primary text-sm font-semibold pt-2">
+                  Bootcamp
+                </div>
+                <div className="pt-3 text-dark text-xl font-semibold">
+                  UI/UX Design
+                </div>
+                <div className="pt-1 text-xs text-dark">
+                  Mulai karirmu di bidang ui/ux dengan program training bootcamp
+                  kami selama 12 minggu dan dapatkan fasilitas koneksi kerja
+                  seumur hidup ke 1.000+ perusahaan hiring partner Purwadhika
+                  yang tersebar di Asia.
+                </div>
+                <div className="py-2">
+                  <Link
+                    href="/post/Design"
+                    className="pt-1 bg-primary py-1 flex justify-center rounded-lg hover:shadow-lg hover:bg-teal-400 text-base text-white font-bold"
+                  >
+                    Lihat Detail
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="max-w-64 border border-solid border-2 border-primary p-2 rounded-lg">
+                <div>
+                  <Image
+                    src="/data-science.jpg"
+                    alt="logo"
+                    width={250}
+                    height={250}
+                    className="rounded-lg"
+                  />
+                </div>
+                <div className="text-primary text-sm font-semibold py-2">
+                  Bootcamp
+                </div>
+                <div className="text-dark text-xl font-semibold">
+                  Data Science
+                </div>
+                <div className="pt-1 text-sm text-dark">
+                  Lulusan Job Connector Bootcamp dipastikan akan mendapatkan
+                  fasilitas job connection seumur hidup baik untuk pekerjaan
+                  full time.
+                </div>
+                <div className="py-2">
+                  <Link
+                    href="/post/Science"
+                    className="bg-primary py-1 flex justify-center rounded-lg hover:shadow-lg hover:bg-teal-400 text-base text-white font-bold"
+                  >
+                    Lihat Detail
+                  </Link>
+                </div>
+              </div>
             </div>
           </div>
         </section>
